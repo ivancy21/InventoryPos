0410d8ecd8d33774ef55d208fab5916d6c2f9b17
ivancy21 <rexivancy21@gmail.com>
2019-11-23 13:33:53 +0800

    layout changes

--- resources/views/Panels/Medicine/medListCreate.blade.php
+++ resources/views/Panels/Medicine/medListCreate.blade.php
@@ -4,122 +4,103 @@
 @include('Layouts.verticalSideBar')
 @include('Layouts.cropImageModal')
 
-<link href="https://res.cloudinary.com/mdrecords/raw/upload/v1561439126/jquery.signaturepad.css" rel="stylesheet">
-<script type="text/javascript" src="https://res.cloudinary.com/mdrecords/raw/upload/v1561439191/json2.min.js" defer>
-</script>
-<script type="text/javascript" src="https://res.cloudinary.com/mdrecords/raw/upload/v1561439191/numeric-1.2.6.min.js"
-    defer></script>
-<script type="text/javascript" src="https://res.cloudinary.com/mdrecords/raw/upload/v1561439179/bezier.js" defer>
-</script>
-<script type="text/javascript" src="https://res.cloudinary.com/mdrecords/raw/upload/v1561439185/html2canvas.js" defer>
-</script>
-<script type="text/javascript" src="https://res.cloudinary.com/mdrecords/raw/upload/v1561439191/jquery.signaturepad.js"
-    defer></script>
 
-<link href="{{ asset('css/croppie.css') }}" rel="stylesheet" />
-<script type="text/javascript" src="{{ asset('js/croppie.js') }}" defer></script>
 <div class="content-container">
-      <form class="form-horizontal" method="POST" action="{{route('medicine.store')}}">
-            @csrf
-           <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+    <form class="form-horizontal" method="POST" action="{{route('medicine.store')}}">
+        @csrf
+        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
            
 
-<div class="container">
-     <div class="row">
-            <div class="col-sm-4">
-                <div class="HeaderBanner p-2 px-3" style="border-radius: .75rem .75rem 0rem 0rem; letter-spacing: 1px;">
-                        <span class="HeaderBannerText">Insert Picture</span>
-                </div>
-
-                <div class="DivTemplate">
-                        <form class="md-form">
-                                <div class="file-field">
-                                  <div class="z-depth-1-half mb-4">
-                                      <img src="{{ asset('images/medicinePhotos/default pic.jpg') }}" id= "Photo" size="250px" width="300px" alt="" class="img-fluid"
-                                      alt="example placeholder">
-                                  </div>
-                                  <div class="d-flex justify-content-center">
-                                    <div class="btn btn-mdb-color btn-rounded float-left">
-                                      <span>Choose file</span>
-                                          
-                                      <input type="file" id='medicinePhoto'
-                                      class="form-control{{ $errors->has('medicinePhoto') ? ' is-invalid' : '' }}"
-                                      name='medicinePic' style="border: none" />
-                                      <input type="hidden" id="medicinePhotos" name="medicinePhoto">
-                                  </div>
-                                  </div>
-                                </div>
-                              </form>
-                    </div>
-            </div>
-    
-  <div class="col-sm-8">
+            <div class="container">
+                <div class="row">
+                        <div class="col-sm-4">
                             <div class="HeaderBanner p-2 px-3" style="border-radius: .75rem .75rem 0rem 0rem; letter-spacing: 1px;">
-                                    <span class="HeaderBannerText">Details</span>
+                                    <span class="HeaderBannerText">Insert Picture</span>
                             </div>
-
-
-                        <div class="DivTemplate">
-                                <div class="row">
-                                    <div class="col">
-                                          <label  class="fnt">Medicine Code</label>
-                                          <input type="text" id="productCode" class="form-control" name="productCode">
-                                    </div>
-                             
-                                    <div class="col">
-                                          <label  class="fnt">Medicine Name</label>
-                                          <input type="text" id="name" class="form-control" name="name" >
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col">
-                                            <label  class="fnt">Category</label>
-                                            <input type="text" id="category" class="form-control" name="category" >
-                                    </div>
-                            
-                                    <div class="col">
-                                            <label  class="fnt">Selling Price</label>
-                                            <input type="text" id="sellingPrice" class="form-control" name="price" >
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col">
-                                            <label  class="fnt">Generic Name</label>
-                                            <input type="text" id="genericName" class="form-control" name="genericName" >
-                                    </div>
-                            
-                                  <div class="col">
-                                        <label  class="fnt">Company Name</label>
-                                        <input type="text" id="companyName" class="form-control" name="companyName" >
-                                    </div>
-                                </div>
-                                <div class="row">
-                                  <div class="col-sm-6">
-                                        <label  class="fnt">Side Effect</label>       
-                                        <input type="text" id="effects" class="form-control" name="sideEffects" >
-                                    </div>
+                            <div class="flex HeaderBody">
+                                    <form class="md-form">
+                                            <div class="file-field">
+                                            <div class="z-depth-1-half mb-1">
+                                                <img src="{{ asset('images/medicineicon.png') }}" size="250px" id="Photo"  alt="" class="img-fluid img-size"
+                                                alt="example placeholder">
+                                            </div>
+                                            <div class="d-flex justify-content-center">
+                                                <div class="btn btn-mdb-color btn-rounded float-left">
+                                                <span>Choose file</span>
+                                                    
+                                                <input type="file" id='medicinePhoto'
+                                                class="form-control{{ $errors->has('medicinePhoto') ? ' is-invalid' : '' }}"
+                                                name='medicinePic' style="border: none" />
+                                                <input type="hidden" id="medicinePhotos" name="medicinePhoto">
+                                            </div>
+                                            </div>
+                                            </div>
+                                    </form>
                                 </div>
                         </div>
-                    
+                
+                      <div class="col-sm-8">
+                                    <div class="HeaderBanner p-2 px-3" style="border-radius: .75rem .75rem 0rem 0rem; letter-spacing: 1px;">
+                                            <span class="HeaderBannerText">Details</span>
+                                    </div>
+                                    <div class="flex HeaderBody">
+                                        <div class="row">
+                                            <div class="col">
+                                                <label  class="fnt">Medicine Code</label>
+                                                <input type="text" id="productCode" class="form-control" name="productCode" required>
+                                            </div>
+                                    
+                                            <div class="col">
+                                                <label  class="fnt">Medicine Name</label>
+                                                <input type="text" id="name" class="form-control" name="name" required>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col">
+                                                    <label  class="fnt">Category</label>
+                                                    <input type="text" id="category" class="form-control" name="category" required>
+                                            </div>
+                                    
+                                            <div class="col">
+                                                    <label  class="fnt">Selling Price</label>
+                                                    <input type="text" id="sellingPrice" class="form-control" name="price"  pattern="[0-9]+" title="Number only" required>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col">
+                                                    <label  class="fnt">Generic Name</label>
+                                                    <input type="text" id="genericName" class="form-control" name="genericName" required>
+                                            </div>
+                                    
+                                        <div class="col">
+                                                <label  class="fnt">Company Name</label>
+                                                <input type="text" id="companyName" class="form-control" name="companyName" required>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                        <div class="col-sm-6">
+                                                <label  class="fnt">Side Effect</label>       
+                                                <input type="text" id="effects" class="form-control" name="sideEffects" required>
+                                            </div>
+                                        </div>
+                                </div>
+                            
 
-                      <div class="DivTemplate">
-                          <p class='DivHeaderText' style="font-size:9px;">ACTIONS</p>
-                          <div class="hr mb-2"></div> 
-                          <button type="submit" class="btn btn-primary">SAVE</button>
-                          <input class="btn btn-outline-info waves-effect float-right" type="button" href="/MedList" value="BACK">    
+                            <div class="DivTemplate">
+                                <p class='DivHeaderText' style="font-size:9px;">ACTIONS</p>
+                                <div class="hr mb-2"></div> 
+                                <button type="submit" class="btn btn-primary">SAVE</button>
+                                <input class="btn btn-outline-info waves-effect float-right" type="button" onclick="window.location='{{route('medicine.index')}}'" value="BACK">    
+                            </div>
                       </div>
-
-                    </div>
-
-
-
-                    </div>              
-
-
+                 </div>
             </div>
-      </div>
+        </div>
+    </form>
 </div>
-</div>
+
+
+    
 <script>
   $(document).ready(function () {
       //Crop image
--- resources/views/Panels/Medicine/medListEdit.blade.php
+++ resources/views/Panels/Medicine/medListEdit.blade.php
@@ -4,126 +4,107 @@
 @include('Layouts.verticalSideBar')
 @include('Layouts.cropImageModal')
 
-<link href="https://res.cloudinary.com/mdrecords/raw/upload/v1561439126/jquery.signaturepad.css" rel="stylesheet">
-<script type="text/javascript" src="https://res.cloudinary.com/mdrecords/raw/upload/v1561439191/json2.min.js" defer>
-</script>
-<script type="text/javascript" src="https://res.cloudinary.com/mdrecords/raw/upload/v1561439191/numeric-1.2.6.min.js"
-    defer></script>
-<script type="text/javascript" src="https://res.cloudinary.com/mdrecords/raw/upload/v1561439179/bezier.js" defer>
-</script>
-<script type="text/javascript" src="https://res.cloudinary.com/mdrecords/raw/upload/v1561439185/html2canvas.js" defer>
-</script>
-<script type="text/javascript" src="https://res.cloudinary.com/mdrecords/raw/upload/v1561439191/jquery.signaturepad.js"
-    defer></script>
 
-<link href="{{ asset('css/croppie.css') }}" rel="stylesheet" />
-<script type="text/javascript" src="{{ asset('js/croppie.js') }}" defer></script>
 <div class="content-container">
       <form class="form-horizontal" method="POST" action="{{route('medicine.update',$medicine->id)}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-           <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+           @csrf
+           @method('PUT')
+      <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
            
 
-<div class="container">
-     <div class="row">
-            <div class="col-sm-4">
-                <div class="HeaderBanner p-2 px-3" style="border-radius: .75rem .75rem 0rem 0rem; letter-spacing: 1px;">
-                        <span class="HeaderBannerText">Insert Picture</span>
-                </div>
-
-                <div class="DivTemplate">
-                        <form class="md-form">
-                                <div class="file-field">
-                                  <div class="z-depth-1-half mb-4">
-                                      < @if ($medicine->medicinePhoto != null)
-                                      <img src="{{ asset('images/medicinePhotos/'.$medicine->medicinePhoto) }}" size ="300px" width ="300px" alt="">
-                                      @else
-                                      <img src="{{ asset('images/medicinePhotos/default pic.jpg') }}"  id="Photo" size="300px" width="300px" alt="">
-                                      @endif
-                                  </div>
-                                  <div class="d-flex justify-content-center">
-                                    <div class="btn btn-mdb-color btn-rounded float-left">
-                                      <span>Choose file</span>
-                                          
-                                      <input type="file" id='medicinePhoto' onchange="readURL(this);"
-                                      class="form-control{{ $errors->has('medicinePhoto') ? ' is-invalid' : '' }}"
-                                      name='medicinePic' style="border: none" />
-                                      <input type="hidden" id="medicinePhotos" name="medicinePhoto">
-                                  </div>
-                                  </div>
-                                </div>
-                              </form>
-                    </div>
-            </div>
-    
-  <div class="col-sm-8">
+        <div class="container">
+                 <div class="row">
+                    <div class="col-sm-4">
                             <div class="HeaderBanner p-2 px-3" style="border-radius: .75rem .75rem 0rem 0rem; letter-spacing: 1px;">
-                                    <span class="HeaderBannerText">Details</span>
+                                    <span class="HeaderBannerText">Insert Picture</span>
                             </div>
-
-
-                        <div class="DivTemplate">
-                                <div class="row">
-                                    <div class="col">
-                                          <label  class="fnt">Medicine Code</label>
-                                    <input type="text" id="productCode" class="form-control" name="productCode" value="{{$medicine->productCode}}">
-                                    </div>
-                             
-                                    <div class="col">
-                                          <label  class="fnt">Medicine Name</label>
-                                          <input type="text" id="name" class="form-control" name="name" value="{{$medicine->name}}" >
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col">
-                                            <label  class="fnt">Category</label>
-                                            <input type="text" id="category" class="form-control" name="category" value="{{$medicine->category}}">
-                                    </div>
-                            
-                                    <div class="col">
-                                            <label  class="fnt">Selling Price</label>
-                                            <input type="text" id="sellingPrice" class="form-control" name="price" value="{{$medicine->price}}">
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col">
-                                            <label  class="fnt">Generic Name</label>
-                                            <input type="text" id="genericName" class="form-control" name="genericName" value="{{$medicine->genericName}}" >
-                                    </div>
-                            
-                                  <div class="col">
-                                        <label  class="fnt">Company Name</label>
-                                        <input type="text" id="companyName" class="form-control" name="companyName" value="{{$medicine->companyName}}">
-                                    </div>
-                                </div>
-                                <div class="row">
-                                  <div class="col-sm-6">
-                                        <label  class="fnt">Side Effect</label>       
-                                        <input type="text" id="effects" class="form-control" name="sideEffects" value="{{$medicine->sideEffects}}">
-                                    </div>
-                                </div>
+                        <div class="flex HeaderBody">
+                                <form class="md-form">
+                                        <div class="file-field">
+                                            <div class="z-depth-1-half mb-4">
+                                                @if ($medicine->medicinePhoto != null)
+                                                <img src="{{ asset('images/medicinePhotos/'.$medicine->medicinePhoto) }}" size="250px" id="Photo" class="img-fluid img-size" alt="">
+                                                @else
+                                                <img src="{{ asset('images/medicineicon.png') }}" size="250px"  class="img-fluid img-size" alt="">
+                                                @endif
+                                            </div>
+                                            <div class="d-flex justify-content-center">
+                                                    <div class="btn btn-mdb-color btn-rounded float-left">
+                                                        <span>Choose file</span>
+                                                            
+                                                        <input type="file" id='medicinePhoto' onchange="readURL(this);"
+                                                        class="form-control{{ $errors->has('medicinePhoto') ? ' is-invalid' : '' }}"
+                                                        name='medicinePic' style="border: none" />
+                                                        <input type="hidden" id="medicinePhotos" name="medicinePhoto">
+                                                    </div>
+                                            </div>
+                                        </div>
+                                </form>
                         </div>
-                    
-
-                      <div class="DivTemplate">
-                          <p class='DivHeaderText' style="font-size:9px;">ACTIONS</p>
-                          <div class="hr mb-2"></div> 
-                          <button type="submit" class="btn btn-primary">SAVE</button>
-                          <input class="btn btn-outline-info waves-effect float-right" type="button" href="/MedList" value="BACK">    
-                      </div>
+                    </div>
+            
+                   <div class="col-sm-8">
+                                <div class="HeaderBanner p-2 px-3" style="border-radius: .75rem .75rem 0rem 0rem; letter-spacing: 1px;">
+                                        <span class="HeaderBannerText">Details</span>
+                                </div>
+                                <div class="flex HeaderBody">
+                                    <div class="row">
+                                        <div class="col">
+                                            <label  class="fnt">Medicine Code</label>
+                                        <input type="text" id="productCode" class="form-control" name="productCode" value="{{$medicine->productCode}}" required>
+                                        </div>
+                                
+                                        <div class="col">
+                                            <label  class="fnt">Medicine Name</label>
+                                            <input type="text" id="name" class="form-control" name="name" value="{{$medicine->name}}" required>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col">
+                                                <label  class="fnt">Category</label>
+                                                <input type="text" id="category" class="form-control" name="category" value="{{$medicine->category}}" required>
+                                        </div>
+                                
+                                        <div class="col">
+                                                <label  class="fnt">Selling Price</label>
+                                                <input type="text" id="sellingPrice" class="form-control" name="price" value="{{$medicine->price}}" pattern="[0-9]+" title="Number only" required>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col">
+                                                <label  class="fnt">Generic Name</label>
+                                                <input type="text" id="genericName" class="form-control" name="genericName" value="{{$medicine->genericName}}" required>
+                                        </div>
+                                
+                                    <div class="col">
+                                            <label  class="fnt">Company Name</label>
+                                            <input type="text" id="companyName" class="form-control" name="companyName" value="{{$medicine->companyName}}" required>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                    <div class="col-sm-6">
+                                            <label  class="fnt">Side Effect</label>       
+                                            <input type="text" id="effects" class="form-control" name="sideEffects" value="{{$medicine->sideEffects}}" required>
+                                    </div>
+                                    </div>
+                            </div>
+                        
+                                <div class="DivTemplate">
+                                    <p class='DivHeaderText' style="font-size:9px;">ACTIONS</p>
+                                    <div class="hr mb-2"></div> 
+                                    <button type="submit" class="btn btn-primary">SAVE</button>
+                                    <input class="btn btn-outline-info waves-effect float-right" type="button" onclick="window.location='{{route('medicine.index')}}'" value="BACK">    
+                                </div>
 
                     </div>
 
 
 
-                    </div>              
-
-
-            </div>
+                </div>              
+         </div>
       </div>
 </div>
-</div>
+
 <script>
   $(document).ready(function () {
       //Crop image
